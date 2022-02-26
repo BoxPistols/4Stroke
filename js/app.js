4e836a6290f0055b276ae4a5c2e7615ee259696a
aito <BoxPistols@users.noreply.github.com>
2022-02-26 19:58:19 +0900

    Remove GSAP Native-CSS Horizontal-Scroll
    TODO: React化した時に実装する

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+//   gsap.registerPlugin(ScrollTrigger);
+//   let sections = gsap.utils.toArray(".garage");
+//   console.log(sections);
+//   gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: "body",
+//       pin: true,
+//       scrub: 1,
+//       snap: 1 / (sections.length - 1),
+//       // base vertical scrolling on how wide the container is so it feels more natural.
+//       end: "+=1000",
+//     },
+//   });
+
   /**
    * 汎用関数
    * @_x = ターゲットエレメント
